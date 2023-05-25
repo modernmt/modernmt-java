eb939cdc52ba12abd51c071ea91406dd983f66c6
Davide Conti <davide.conti@modernmt.eu>
2023-05-25 13:46:25 +0200

    Add support for quality estimation endpoint

---
+++ src/main/java/com/modernmt/model/QualityEstimation.java
@@ -0,0 +1,11 @@
+package com.modernmt.model;
+
+public class QualityEstimation extends Model {
+
+        private float score;
+
+        public float getScore() {
+            return score;
+        }
+
+}
